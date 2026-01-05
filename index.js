08a1aa5354e42bf5bb6fdc1cde2e0fb6d4ee8149
Usama <usamamuhammadharuna6@gmail.com>
2026-01-06 00:30:15 +0100

    second commit

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ barBtn.addEventListener('click', () => {
 })
 
 // get started button logic
+
 const getsBtn = document.querySelector('.get-s-btn');
 
 getsBtn.addEventListener('mouseover', () => {
@@ -23,4 +24,55 @@ getsBtn.addEventListener('mouseover', () => {
 })
 getsBtn.addEventListener('mouseout', () => {
     getsBtn.classList.toggle('get-s-btn-hover')
+})
+
+// get started button logic for small screen
+
+const getsBtnSm = document.querySelector('.get-s-btn-sm');
+
+getsBtnSm.addEventListener('mouseover', () => {
+    getsBtnSm.classList.toggle('get-s-btn-hover')
+})
+getsBtnSm.addEventListener('mouseout', () => {
+    getsBtnSm.classList.toggle('get-s-btn-hover')
+})
+
+
+/*
+   =====================================================
+   ================== HERO SECTION =====================
+   ===================================================== 
+   */
+
+// background omage size small screen logic
+const heroBg = document.querySelector('.hero-bg');
+
+if (window.innerWidth <= 992 + "px") {
+    heroBg.style.backgroundImage.src = url('images/hero-img.png2');
+}
+
+// button 1 logic
+
+const heroBtn1 = document.querySelector('.hero-title-btn1');
+
+heroBtn1.addEventListener('mouseover', () => {
+    heroBtn1.classList.toggle('hero-title-btn1-hover');
+})
+heroBtn1.addEventListener('mouseout', () => {
+    heroBtn1.classList.toggle('hero-title-btn1-hover');
+})
+
+// button 2 logic
+
+const heroBtn2 = document.querySelector('.hero-title-btn2');
+
+heroBtn2.addEventListener('mouseover', () => {
+    heroBtn1.classList.toggle('hero-title-btn2-hover');
+    heroBtn2.style.backgroundColor = "#ffff";
+    heroBtn2.style.color = "#000";
+})
+heroBtn2.addEventListener('mouseout', () => {
+    heroBtn2.classList.toggle('hero-title-btn2-hover');
+    heroBtn2.style.backgroundColor = "transparent";
+    heroBtn2.style.color = "#ffff";
 })
